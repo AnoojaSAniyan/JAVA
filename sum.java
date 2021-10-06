0faf02c36cc90c672f44b26de78d777b2c7bd56f
AnoojaSAniyan <84064868+AnoojaSAniyan@users.noreply.github.com>
2021-10-06 12:17:42 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.Scanner;
+class sum
+{
+public static void main(String[]args)
+{
+int a,b,n;
+int sum;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the number");
+a=sc.nextInt();
+for(i=0;<n;i++)
+{
+for(j=0;j<nj++)
+}
+System.out.println("sum is:"+"a+"+"b");
+b=sc.nextInt();
+}
+}
+
+
